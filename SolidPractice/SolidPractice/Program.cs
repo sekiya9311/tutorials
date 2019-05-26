dfc0544fa80fa7060261cb63bc3aebbaa871897f
sekiya9311 <sekiya9311@gmail.com>
2019-05-26 11:01:47 +0900

    Add project of SolidPractice

---
+++ SolidPractice/SolidPractice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SolidPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
