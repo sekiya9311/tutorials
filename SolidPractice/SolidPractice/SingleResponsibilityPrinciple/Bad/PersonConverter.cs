59d2bcd03222c73d853a80959f71bc9d06741114
sekiya9311 <sekiya9311@gmail.com>
2019-05-27 01:24:09 +0900

    Add bad sample

---
+++ SolidPractice/SolidPractice/SingleResponsibilityPrinciple/Bad/PersonConverter.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace SolidPractice.SingleResponsibilityPrinciple.Bad
+{
+    public class PersonConverter
+    {
+        public void CsvToJson(Stream inputStream, Stream outputStream)
+        {
+            // 入力ストリームからCSVデータの読み取り
+            var lines = new List<string>();
+            using (var reader = new StreamReader(inputStream))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    lines.Add(line);
+                }
+            }
+
+            // CSVデータを解析し、Entityに変換
+            var persons = new List<Person>();
+            foreach (var line in lines)
+            {
+                var fields = line.Split(",".ToCharArray());
+
+                if (fields.Length != 4)
+                {
+                    Console.WriteLine("invalid");
+                    continue;
+                }
+                if (!int.TryParse(fields[1], out var age))
+                {
+                    Console.WriteLine("invalid");
+                    continue;
+                }
+                if (!double.TryParse(fields[2], out var height))
+                {
+                    Console.WriteLine("invalid");
+                    continue;
+                }
+                if (!double.TryParse(fields[3], out var weight))
+                {
+                    Console.WriteLine("invalid");
+                    continue;
+                }
+
+                var person = new Person()
+                {
+                    Name = fields[0],
+                    Age = age,
+                    Height = height,
+                    Weight = weight
+                };
+                persons.Add(person);
+            }
+
+            // Entityデータをjsonに変換し出力ストリームに出力
+            using (var writer = new StreamWriter(outputStream))
+            {
+                var personArrayOfJson = JsonConvert.SerializeObject(persons);
+                writer.WriteLine(personArrayOfJson);
+            }
+        }
+    }
+}
---
+++ SolidPractice/SolidPractice/SingleResponsibilityPrinciple/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SolidPractice.SingleResponsibilityPrinciple
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public double Height { get; set; }
+        public double Weight { get; set; }
+    }
+}
