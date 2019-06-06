077e58c83a0f33f1b6ab3608413acd0bc3e7bd83
sekiya9311 <sekiya9311@gmail.com>
2019-06-07 00:49:33 +0900

    Add splitter

---
+++ SolidPractice/SolidPractice/SingleResponsibilityPrinciple/Good/impl/PersonSplitterPlain.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using SolidPractice.SingleResponsibilityPrinciple.Good.interfaces;
+
+namespace SolidPractice.SingleResponsibilityPrinciple.Good.impl
+{
+    public class PersonSplitterPlain : IPersonSplitter
+    {
+        public IEnumerable<string> Split(string line)
+            => line.Split(new[] { ',' });
+    }
+}
---
+++ SolidPractice/SolidPractice/SingleResponsibilityPrinciple/Good/interfaces/IPersonSplitter.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SolidPractice.SingleResponsibilityPrinciple.Good.interfaces
+{
+    public interface IPersonSplitter
+    {
+        IEnumerable<string> Split(string line);
+    }
+}
