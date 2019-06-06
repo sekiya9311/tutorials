629dfca3b85fb2b05ebcea77ade7e67c7b0e7933
sekiya9311 <sekiya9311@gmail.com>
2019-06-07 00:48:54 +0900

    Modify person class

--- SolidPractice/SolidPractice/SingleResponsibilityPrinciple/Person.cs
+++ SolidPractice/SolidPractice/SingleResponsibilityPrinciple/Person.cs
@@ -10,5 +10,18 @@ namespace SolidPractice.SingleResponsibilityPrinciple
         public int Age { get; set; }
         public double Height { get; set; }
         public double Weight { get; set; }
+
+        public override bool Equals(object obj)
+            => (obj is Person p) &&
+                p.Name == this.Name &&
+                p.Age == this.Age &&
+                p.Height == this.Height &&
+                p.Weight == this.Weight;
+
+        public override int GetHashCode()
+            => this.Name.GetHashCode() ^
+                this.Age.GetHashCode() ^
+                this.Height.GetHashCode() ^
+                this.Weight.GetHashCode();
     }
 }
