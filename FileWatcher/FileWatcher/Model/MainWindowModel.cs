bf8e5ced216ff436502ce8823d75ad09b950ffb4
sekiya9311 <sekiya9311@gmail.com>
2019-06-19 23:06:55 +0900

    impl

--- FileWatcher/FileWatcher/Model/MainWindowModel.cs
+++ FileWatcher/FileWatcher/Model/MainWindowModel.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Reactive.Bindings;
 
 namespace FileWatcher.Model
 {
     class MainWindowModel
     {
+
+        private readonly string DirectoryPath = Directory.GetCurrentDirectory();
+        private readonly string FileFilter = "*.txt";
+        public FileSystemWatcher Watcher { get; }
+
+        public MainWindowModel()
+        {
+            Watcher = new FileSystemWatcher(DirectoryPath, FileFilter)
+            {
+                EnableRaisingEvents = true
+            };
+        }
     }
 }
--- FileWatcher/FileWatcher/ViewModel/MainWindowViewModel.cs
+++ FileWatcher/FileWatcher/ViewModel/MainWindowViewModel.cs
@@ -3,10 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reactive.Linq;
+using Reactive.Bindings;
+
+using FileWatcher.Model;
+using FileWatcher.Util;
 
 namespace FileWatcher.ViewModel
 {
     class MainWindowViewModel
     {
+        private readonly MainWindowModel _model;
+
+        public ReadOnlyReactiveProperty<string> WatchFileState { get; }
+
+        public MainWindowViewModel()
+        {
+            _model = new MainWindowModel();
+
+            WatchFileState = Observable.Merge(
+                _model.Watcher.CreatedToObservable(),
+                _model.Watcher.DeletedToObservable(),
+                _model.Watcher.ChangedToObservable(),
+                _model.Watcher.RenamedToObservable()).Select(e =>
+                {
+                    switch (e.ChangeType)
+                    {
+                        case System.IO.WatcherChangeTypes.Created:
+                            return "新しく作られた！";
+                        case System.IO.WatcherChangeTypes.Deleted:
+                            return "消された！";
+                        case System.IO.WatcherChangeTypes.Changed:
+                            return "なんか変わった！";
+                        case System.IO.WatcherChangeTypes.Renamed:
+                            return "名前変わった！";
+                        case System.IO.WatcherChangeTypes.All:
+                            return "全部!?";
+                        default:
+                            return "知らない子ですね";
+                    }
+                }).ToReadOnlyReactiveProperty();
+        }
     }
 }
